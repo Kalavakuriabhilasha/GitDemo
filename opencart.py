d098e0587059b89ff27cee8e9e7b6e6c6c02c251
Abhilasha <Abhilasha.kalavakuri@ibm.com>
2024-12-11 15:01:44 +0530

    this is commit from assian guy

--- opencart.py
+++ opencart.py
@@ -7,3 +7,4 @@ driver.get("https://demo.opencart.com/en-gb?route=account/register")
 driver.maximize_window()
 time.sleep(10)
 driver.close()
+
--- pattern.py
+++ pattern.py
@@ -3,3 +3,9 @@ for i in range (n):
     for j in range (i):
        print(i,end = "")
     print()
+
+n = 6
+for i in range (n):
+    for j in range (i):
+       print(i,end = "")
+    print()
